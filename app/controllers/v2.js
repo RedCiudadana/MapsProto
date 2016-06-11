f288e72ac0921284b3ee12b008bab1865d43393d
Hector Hurtarte <hhurtarte@servirsa.com>
2016-06-11 08:52:03 +0100

    WIP: Trabajar en despliegue de mapa

---
+++ app/controllers/v2.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  currentLatitude: 14.569668,
+  currentLongitude: -90.497174,
+  currentZoom: 13,
+  currentEscuela: null
+});
