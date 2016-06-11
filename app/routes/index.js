973b3ed92c93013d652d266fa1e8fce44d56fec4
Hector Hurtarte <hhurtarte@servirsa.com>
2016-06-11 08:19:46 +0100

    WIP: Implementar version 2

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
   model() {
-    return Ember.RSVP.hash({
-      escuelas: this.store.findAll('escuela')
-    });
+    return this.modelFor('application');
   },
 
   setupController(controller, model) {
