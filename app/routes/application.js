64394535640f8ec63471dd9731ce5538897293d9
Hector Hurtarte <hhurtarte@servirsa.com>
2016-06-11 09:30:58 +0100

    WIP: Add 'loading' template

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      escuelas: this.store.findAll('escuela')
+      escuelas: this.store.findAll('escuela').then(() => {
+        console.log('done loading model for application');
+      })
     });
   }
 });
--- app/routes/v2.js
+++ app/routes/v2.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.modelFor('application');
+  },
+  actions: {
+    loading(transition, route) {
+      console.log('in loading');
+    }
   }
 });
